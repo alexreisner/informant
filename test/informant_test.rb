1ae0f6237d01f1d5e9a656f0d1472ac1ed04e324
Alex Reisner <alex@alexreisner.com>
2009-08-01 16:02:45 -0400

    First attempt at testing (not working yet).

--- test/informant_test.rb
+++ test/informant_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
-class InformantTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+class InformantTest < ActionView::TestCase
+
+  test "text field" do
+    # object_name, object, self, options, block
+    p = lambda{ |b| b.text_field(:color) }
+    b = Informant::Standard.new(:car, Object.new, @controller, {}, p)
+    assert_equal "", b
   end
 end
