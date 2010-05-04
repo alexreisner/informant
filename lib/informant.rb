701b59a5c2269b18833f207f01b3cf59015ccef7
Alex Reisner <alex@alexreisner.com>
2010-05-03 22:50:09 -0400

    Make label content HTML safe.

--- lib/informant.rb
+++ lib/informant.rb
@@ -202,9 +202,10 @@ module Informant
       options.delete :label_for
       options.delete :required
       
-      text = text.blank?? method.to_s.humanize : text.to_s
-      text += ':' if colon
-      text += ' <span class="required">*</span>' if required
+      text = text.blank?? method.to_s.humanize : h(text.to_s)
+      text << ':' if colon
+      text << @template.content_tag(:span, "*", :class => "required") if required
+      text = text.html_safe
       super
     end
     
